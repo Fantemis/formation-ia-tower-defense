2d6740b2129145630244b11305a5a654debfd258
Alexandre <alexandre@brde.fr>
2021-04-28 17:11:23 +0200

    landmine

--- scripts/Tower.gd
+++ scripts/Tower.gd
@@ -1,6 +1,8 @@
 extends TileMapEntity
 class_name Tower
 
+var timer
+export (int) var duration = 2
 var menu_index
 export (int) var max_health = 500
 
@@ -8,3 +10,24 @@ export (int) var max_health = 500
 func _ready():
 	collision_layer = 0b10
 	collision_mask = 0
+	timer = Timer.new()
+	add_child(timer)
+	timer.one_shot = true
+	#timer.connect("timeout", self, "_on_timer")
+	#timer.start(duration)
+	connect("area_entered", self, "_on_hit")
+	
+func _on_timer():
+	queue_free()
+	
+func _on_hit(other):
+	print_debug("boum ?")
+	if other.has_method("take_damage"):
+		$"AnimatedSprite".show()
+		$"AnimatedSprite".play()
+		other.take_damage(500)
+		$"Sprite2".hide()
+		timer.connect("timeout", self, "_on_timer")
+		timer.start(duration)
+
+
